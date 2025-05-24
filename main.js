9cbe3be9cc9d9657c80432f5501d286a403a1523
antonio-de-ombra <antondzhupyna250208@gmail.com>
2025-05-24 12:00:56 +0200

    lesson16

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
 let menu = document.querySelector(".fixed-menu");
 
 menu.addEventListener('click', function(event){
-   let clickTarget = event.target;
-   let activeBtn = document.querySelector('.active');
-   
-   if (clickTarget.classList.contains('nav-link')){
-       clickTarget.classList.add('active');
-       activeBtn.classList.remove('active');
-       let mobile = document.querySelector('.mobile-menu');
-       if (!mobile.classList.contains('hide')){
-           mobile.classList.add('hide');
-       }
-   }
+  let clickTarget = event.target;
+  let activeBtn = document.querySelector('.active');
+  if (clickTarget.classList.contains('nav-link') ){
+      clickTarget.classList.add('active');
+      activeBtn.classList.remove('active');
+
+      let mobile = document.querySelector('.mobile-menu');
+      if (!mobile.classList.contains('hide')){
+          mobile.classList.add('hide');
+      }
+
+  }
 
    let body = document.querySelector("body");
-   let mobileMenu = dosument.querySelector("mobile-menu");
-   if (!body.classList.contains("off-scroll") &&
-        !mobileMenu.classList.contains('hide')){
-          body.classList.add('off-scroll');
-        }
-    else{
-      body.classList.remove("off-scroll");
-    }
+   let mobileMenu = document.querySelector (".mobile-menu");
+   if (!body.classList.contains ("off-scroll") && !mobileMenu.classList.contains('hide')){
+    body.classList.add("off-scroll");
+   }
+
+   else{
+    body.classList.remove("off-scroll");
+   }
 });
 
 let classLink = '.main-link';
 window.onscroll = function() {
  let h = document.documentElement.clientHeight;
-
  if (window.scrollY >= h*4 ){
    classLink = '.comments-link';
  }
@@ -45,7 +45,7 @@ window.onscroll = function() {
  }
  let activeBtn = document.querySelector('.active');
  let newActiveBtn = document.querySelector(classLink);
- if (!newActiveBtn.classList.contains("active")){
+ if (!newActiveBtn.classList.contains('active')){
    newActiveBtn.classList.add('active');
    activeBtn.classList.remove('active')
  }
@@ -55,9 +55,6 @@ window.onscroll = function() {
 document.querySelector(".mobile-button").addEventListener("click", function(event)
 {
   document.querySelector(".mobile-menu").classList.toggle("hide");
-  document.querySelectorAll(".mobile-btn"). forEach(function(item){
-    item.classList.toggle('btn-hide');
-  })
 });
 
 
